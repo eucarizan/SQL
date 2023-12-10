63af6b1964fe139b5231b3784e418115adc6bccd
eucarizan <eucarizan@gmail.com>
2023-12-10 23:38:43 +0800

    1: benchmark queries for index performance

---
+++ practical-sql-anthony-debarros/ch08/listing_08-13.sql
@@ -0,0 +1,11 @@
+-- Listing 8-13: Benchmark queries for index performance
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'BROADWAY';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = '52 STREET';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'ZWICKY AVENUE';
+
