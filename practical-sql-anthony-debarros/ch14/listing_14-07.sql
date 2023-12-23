9aeb11b2a4d29f32987873d5b98758a922fdf0cf
eucarizan <eucarizan@gmail.com>
2023-12-23 22:30:47 +0800

    1: matching the second date when present - regexp_matches() with g flag

---
+++ practical-sql-anthony-debarros/ch14/listing_14-07.sql
@@ -0,0 +1,5 @@
+-- Listing 14-7: Using the regexp_matches() function with the 'g' flag
+SELECT crime_id,
+       regexp_matches(original_text, '\d{1,2}\/\d{1,2}\/\d{2}', 'g')
+FROM crime_reports
+ORDER BY crime_id;
