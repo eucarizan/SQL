8f7cb173699079112779cfb717d5509ef4c49844
eucarizan <eucarizan@gmail.com>
2023-12-25 19:35:19 +0800

    1: using a capture group to return only the date

---
+++ practical-sql-anthony-debarros/ch14/listing_14-09.sql
@@ -0,0 +1,6 @@
+-- Listing 14-9: Using a capture group to return only the date
+-- Eliminates the hyphen
+SELECT crime_id,
+       regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})')
+FROM crime_reports
+ORDER BY crime_id;
