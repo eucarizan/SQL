9c1434943f154d42ab7854d3521bbc84a73ac35b
eucarizan <eucarizan@gmail.com>
2023-12-26 20:20:46 +0800

    1: updating all crime_reports columns

--- README.md
+++ README.md
@@ -262,9 +262,11 @@ listing|solution|num
 14|[using the regexp_matches() function with the 'g' flag](./practical-sql-anthony-debarros/ch14/listing_14-07.sql)|167
 14|[using regexp_match() to find the second date](./practical-sql-anthony-debarros/ch14/listing_14-08.sql)|168
 14|[using a capture group to return only the date](./practical-sql-anthony-debarros/ch14/listing_14-09.sql)|169
-14|[matching case numbe, date, crime type, and city](./practical-sql-anthony-debarros/ch14/listing_14-10.sql)|170
+14|[matching case number, date, crime type, and city](./practical-sql-anthony-debarros/ch14/listing_14-10.sql)|170
 14|[retrieving a value from within an array](./practical-sql-anthony-debarros/ch14/listing_14-11.sql)|171
-14|[updating the crime_reports date_1 column](./practical-sql-anthony-debarros/ch14/listing_14-0.sql)|0
+14|[updating the crime_reports date_1 column](./practical-sql-anthony-debarros/ch14/listing_14-12.sql)|172
+<hr/>
+14|[updating all crime_reports columns](./practical-sql-anthony-debarros/ch14/listing_14-13.sql)|173
 <hr/>
 <!-- 14|[](./practical-sql-anthony-debarros/ch14/listing_14-0.sql)|0
 -->
---
+++ practical-sql-anthony-debarros/ch14/listing_14-13.sql
@@ -0,0 +1,39 @@
+-- Listing 14-13: Updating all crime_reports columns
+
+UPDATE crime_reports
+SET date_1 = 
+    (
+      (regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+          || ' ' ||
+      (regexp_match(original_text, '\/\d{2}\n(\d{4})'))[1] 
+          ||' US/Eastern'
+    )::timestamptz,
+             
+    date_2 = 
+    CASE 
+    -- if there is no second date but there is a second hour
+        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})') IS NULL)
+                     AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        THEN 
+          ((regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}'))[1]
+              || ' ' ||
+          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
+              ||' US/Eastern'
+          )::timestamptz 
+
+    -- if there is both a second date and second hour
+        WHEN (SELECT regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})') IS NOT NULL)
+              AND (SELECT regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})') IS NOT NULL)
+        THEN 
+          ((regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})'))[1]
+              || ' ' ||
+          (regexp_match(original_text, '\/\d{2}\n\d{4}-(\d{4})'))[1] 
+              ||' US/Eastern'
+          )::timestamptz 
+    END,
+    street = (regexp_match(original_text, 'hrs.\n(\d+ .+(?:Sq.|Plz.|Dr.|Ter.|Rd.))'))[1],
+    city = (regexp_match(original_text,
+                           '(?:Sq.|Plz.|Dr.|Ter.|Rd.)\n(\w+ \w+|\w+)\n'))[1],
+    crime_type = (regexp_match(original_text, '\n(?:\w+ \w+|\w+)\n(.*):'))[1],
+    description = (regexp_match(original_text, ':\s(.+)(?:C0|SO)'))[1],
+    case_number = (regexp_match(original_text, '(?:C0|SO)[0-9]+'))[1];
