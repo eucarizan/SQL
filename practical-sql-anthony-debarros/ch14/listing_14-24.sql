83db5ed7cb64ebafbb6c8678eeebe1ce1a0cc071
eucarizan <eucarizan@gmail.com>
2023-12-30 23:04:05 +0800

    1: finding speeches where 'defense' follows 'military' with specific distance

--- practical-sql-anthony-debarros/ch14/listing_14-24.sql
+++ practical-sql-anthony-debarros/ch14/listing_14-24.sql
@@ -13,3 +13,18 @@ FROM president_speeches
 WHERE search_speech_text @@ 
       to_tsquery('english', 'military <-> defense')
 ORDER BY speech_date;
+
+-- Bonus: Example with a distance of 2:
+SELECT president,
+       speech_date,
+       ts_headline(speech_text, 
+                   to_tsquery('english', 'military <2> defense'),
+                   'StartSel = <,
+                    StopSel = >,
+                    MinWords=5,
+                    MaxWords=7,
+                    MaxFragments=2')
+FROM president_speeches
+WHERE search_speech_text @@ 
+      to_tsquery('english', 'military <2> defense')
+ORDER BY speech_date;
