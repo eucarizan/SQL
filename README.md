35f7250b702d2b13a866c3124b8e3b85b9857eab
eucarizan <eucarizan@gmail.com>
2024-01-03 21:31:51 +0800

    1: functions specific to making polygons

--- README.md
+++ README.md
@@ -291,6 +291,7 @@ listing|solution|num
 15|[using st_geogfromtext() to create spatial objects](./practical-sql-anthony-debarros/ch15/listing_15-04.sql)|190
 15|[functions specific to making points](./practical-sql-anthony-debarros/ch15/listing_15-05.sql)|191
 15|[functions specific to making linestrings](./practical-sql-anthony-debarros/ch15/listing_15-06.sql)|192
+15|[functions specific to making polygons](./practical-sql-anthony-debarros/ch15/listing_15-07.sql)|193
 <hr/>
 <!-- 15|[](./practical-sql-anthony-debarros/ch15/listing_15-0.sql)|0
 -->
---
+++ practical-sql-anthony-debarros/ch15/listing_15-07.sql
@@ -0,0 +1,16 @@
+-- Listing 15-7: Functions specific to making Polygons
+
+SELECT ST_PolygonFromText('POLYGON((-74.9 42.7, -75.1 42.7,
+                                    -75.1 42.6, -74.9 42.7))', 4326);
+
+SELECT ST_MakePolygon(
+           ST_GeomFromText('LINESTRING(-74.92 42.7, -75.06 42.71,
+                                       -75.07 42.64, -74.92 42.7)', 4326));
+
+SELECT ST_MPolyFromText('MULTIPOLYGON((
+                                       (-74.92 42.7, -75.06 42.71,
+                                        -75.07 42.64, -74.92 42.7),
+                                       (-75.0 42.66, -75.0 42.64,
+                                        -74.98 42.64, -74.98 42.66,
+                                        -75.0 42.66)
+                                      ))', 4326);
