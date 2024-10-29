670ad99337655acd4fbbd0bc6f44eb18d0d05701
eucarizan <eucarizan@gmail.com>
2024-10-29 21:53:04 +0800

    3: electronics store customer - refactor 6

--- hyperskill/projects/electronics-store-customer/s06.sql
+++ hyperskill/projects/electronics-store-customer/s06.sql
@@ -1,23 +1,33 @@
 -- Stage 6: Find the second most cost-effective PCs for each specific RAM value
-SELECT 
-    m.pc_code, 
-    m.model, 
-    m.speed, 
-    m.ram, 
-    m.hd, 
-    m.cd, 
-    m.price
-FROM (
-    SELECT 
-        pc_code,
-        model, 
-        speed,
-        ram, 
-        hd,
-        cd,
-        price,
-        rank() OVER (PARTITION BY ram ORDER BY price DESC) AS rank
+
+WITH pc_rank AS (
+    SELECT pc_code, model, speed, ram, hd, cd, price, rank() OVER (PARTITION BY ram ORDER BY price DESC) AS ranking
     FROM PC
-    ORDER BY ram, rank() OVER (PARTITION BY ram ORDER BY price DESC)
-) AS m
-WHERE m.rank = 2;
+) 
+
+SELECT pc_code, model, speed, ram, hd, cd, price
+FROM pc_rank
+WHERE ranking = 2 
+
+--SELECT 
+--    m.pc_code, 
+--    m.model, 
+--    m.speed, 
+--    m.ram, 
+--    m.hd, 
+--    m.cd, 
+--    m.price
+--FROM (
+--    SELECT 
+--        pc_code,
+--        model, 
+--        speed,
+--        ram, 
+--        hd,
+--        cd,
+--        price,
+--        rank() OVER (PARTITION BY ram ORDER BY price DESC) AS rank
+--    FROM PC
+--    ORDER BY ram, rank() OVER (PARTITION BY ram ORDER BY price DESC)
+--) AS m
+--WHERE m.rank = 2;
