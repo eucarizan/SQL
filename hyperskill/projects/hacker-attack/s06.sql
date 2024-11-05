e864163cd932ca4b20e1ba89fdc9c85406940856
eucarizan <eucarizan@gmail.com>
2024-11-05 18:50:05 +0800

    3: hacker attack - update student, sqlite brute force update

--- hyperskill/projects/hacker-attack/s06.sql
+++ hyperskill/projects/hacker-attack/s06.sql
@@ -1,4 +1,52 @@
 -- Stage 6: Find out the grade code of the students and update the student table. 
+CREATE TABLE score1 ( person_id VARCHAR(9), score INTEGER );
+CREATE TABLE score2 ( person_id VARCHAR(9), score INTEGER );
+CREATE TABLE score3 ( person_id VARCHAR(9), score INTEGER );
+
+.mode csv
+.import person.csv person
+.import teacher.csv teacher
+.import --skip 1 score1.csv score1
+.import --skip 1 score2.csv score2
+.import --skip 1 score3.csv score3
+.mode column
+
+CREATE TABLE student ( person_id VARCHAR(9) PRIMARY KEY, grade_code TEXT );
+
+INSERT INTO student (person_id) SELECT person_id
+FROM person WHERE person_id NOT IN (SELECT person_id FROM teacher);
+
+CREATE TABLE score ( person_id VARCHAR(9), score INTEGER );
+INSERT INTO score (person_id, score)
+SELECT * FROM score1 UNION ALL SELECT * FROM score2 UNION ALL SELECT * FROM score3;
+
+DROP TABLE score1; DROP TABLE score2; DROP TABLE score3;
+
+UPDATE student SET grade_code = 'GD-10'
+WHERE person_id IN (
+    SELECT person_id FROM score
+    GROUP BY person_id HAVING count(score) = 1
+); 
+
+UPDATE student SET grade_code = 'GD-11'
+WHERE person_id IN (
+    SELECT person_id FROM score
+    GROUP BY person_id HAVING count(score) = 2
+); 
+
+UPDATE student SET grade_code = 'GD-12'
+WHERE person_id IN (
+    SELECT person_id FROM score
+    GROUP BY person_id HAVING count(score) = 3
+); 
+
+UPDATE student SET grade_code = 'GD-09'
+WHERE grade_code IS NULL;
+
+SELECT * FROM student ORDER BY person_id LIMIT 5;
+
+-- postgresql, unfinished
+/*
 -- UPDATE student
 -- SET grade_code = CASE WHEN (SELECT count(person_id) OVER(PARTITION BY person_id) FROM 
 
@@ -36,3 +84,4 @@ SELECT
          ELSE 'GD-09' END AS grade_code
 FROM student_score
 ORDER BY person_id;
+*/
