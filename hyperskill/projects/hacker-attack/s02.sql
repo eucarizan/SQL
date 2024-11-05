c62a3ae29d735268f215b5bd79d40df8c34e4dc5
eucarizan <eucarizan@gmail.com>
2024-11-05 09:19:55 +0800

    3: hacker attack - find students, sqlite v1

--- hyperskill/projects/hacker-attack/s02.sql
+++ hyperskill/projects/hacker-attack/s02.sql
@@ -1,20 +1,48 @@
 -- Stage 2: Find out who the students are
-CREATE TABLE teacher (
-    person_id varchar(9),
-    class_code text,
-    CONSTRAINT person_id_key PRIMARY KEY (person_id)
+CREATE TABLE person (
+    person_id VARCHAR(9) PRIMARY KEY,
+    full_name TEXT,
+    address TEXT,
+    building_number TEXT,
+    phone_number TEXT
 );
 
-COPY teacher
-FROM 'C:\YourDirectory\teacher.csv'
-WITH (FORMAT CSV, HEADER);
+CREATE TABLE teacher (
+    person_id VARCHAR(9) PRIMARY KEY,
+    class_code TEXT
+);
+
+.mode csv
+.import --skip 1 teacher.csv teacher
+.mode column
 
 SELECT person_id, full_name
 FROM person
 WHERE person_id NOT IN (SELECT person_id FROM teacher)
-ORDER BY full_name
+ORDER BY person_id
 LIMIT 5;
 
-SELECT count(*)
+SELECT COUNT(person_id)
 FROM person
 WHERE person_id NOT IN (SELECT person_id FROM teacher);
+
+-- postgresql
+-- CREATE TABLE teacher (
+--     person_id varchar(9),
+--     class_code text,
+--     CONSTRAINT person_id_key PRIMARY KEY (person_id)
+-- );
+-- 
+-- COPY teacher
+-- FROM 'C:\YourDirectory\teacher.csv'
+-- WITH (FORMAT CSV, HEADER);
+-- 
+-- SELECT person_id, full_name
+-- FROM person
+-- WHERE person_id NOT IN (SELECT person_id FROM teacher)
+-- ORDER BY full_name
+-- LIMIT 5;
+-- 
+-- SELECT count(*)
+-- FROM person
+-- WHERE person_id NOT IN (SELECT person_id FROM teacher);
