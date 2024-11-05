a35002d69eda0f364b7a6bdeb0d70f7501f93650
eucarizan <eucarizan@gmail.com>
2024-11-05 08:13:41 +0800

    3: hacker attack - create person table, v2

--- hyperskill/projects/hacker-attack/s01.sql
+++ hyperskill/projects/hacker-attack/s01.sql
@@ -1,15 +1,34 @@
 -- Stage 1: Create your first table that contains teachers and students
-CREATE TABLE person (
-    person_id varchar(9),
-    full_name text,
-    address text,
-    building_number text,
-    phone_number text,
-    CONSTRAINT person_id_key PRIMARY KEY (person_id)
+CREATE TABLE "person" (
+    "person_id" VARCHAR(9) PRIMARY KEY,
+    "full_name" TEXT,
+    "address" TEXT,
+    "building_number" TEXT,
+    "phone_number" TEXT
 );
 
-COPY person
-FROM 'C:\YourDirectory\person.csv'
-WITH (FORMAT CSV, HEADER);
+.mode csv
+.import --skip 1 person.csv person
+.mode column
 
-SELECT person_id, full_name FROM person ORDER BY person_id LIMIT 5;
+SELECT person_id, full_name
+FROM person
+ORDER BY person_id
+LIMIT 5;
+
+-- postgresql
+--CREATE TABLE person (
+--    person_id varchar(9),
+--    full_name text,
+--    address text,
+--    building_number text,
+--    phone_number text,
+--    CONSTRAINT person_id_key PRIMARY KEY (person_id)
+--);
+--
+--COPY person
+--FROM 'C:\YourDirectory\person.csv'
+--WITH (FORMAT CSV, HEADER);
+--
+--SELECT person_id, full_name FROM person ORDER BY person_id LIMIT 5;
+
