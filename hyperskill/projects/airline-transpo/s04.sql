0b835cb6384b5b3b180cb727f27a5888e0a42f69
eucarizan <eucarizan@gmail.com>
2024-11-14 22:46:03 +0800

    3: airline transportation - boeing vs airbus comparison, simple

--- hyperskill/projects/airline-transpo/s04.sql
+++ hyperskill/projects/airline-transpo/s04.sql
@@ -1,4 +1,15 @@
 -- Stage 04: Compare flight durations and frequenices between Boeing and Airbus aircraft types
+SELECT
+  CASE
+    WHEN plane_type LIKE 'Boeing%' THEN 'Boeing'
+    WHEN plane_type LIKE 'Airbus%' THEN 'Airbus'
+  END AS aircraft_type,
+  AVG(TIMESTAMPDIFF(MINUTE, time_out, time_in)) AS avg_flight_duration,
+  COUNT(*) AS num_flights
+FROM Trip
+GROUP BY aircraft_type;
+
+/*
 WITH plane_duration AS (
   SELECT 
     CASE
@@ -14,3 +25,4 @@ SELECT
   COUNT(aircraft_type) AS num_flights
 FROM plane_duration
 GROUP BY aircraft_type;
+*/
