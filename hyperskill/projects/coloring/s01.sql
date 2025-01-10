e6237faf664afed8f15cdd3c480a981fd30a0828
eucarizan <eucarizan@gmail.com>
2025-01-10 22:41:43 +0800

    1: coloring - stage 1 refactor, efficiency

--- hyperskill/projects/coloring/s01.sql
+++ hyperskill/projects/coloring/s01.sql
@@ -1,3 +1,10 @@
+SELECT
+  s.id, s.name
+FROM Square s 
+LEFT JOIN Painting p ON s.id = p.square_id
+WHERE p.square_id IS NULL
+ORDER BY s.name DESC;
+/*
 SELECT 
   s.id,
   s.name
@@ -5,3 +12,4 @@ FROM Painting p
 RIGHT JOIN Square s ON p.square_id = s.id
 WHERE s.id NOT IN (SELECT square_id FROM Painting)
 ORDER BY s.name DESC;
+*/
